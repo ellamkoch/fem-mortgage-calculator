5d1f66e5fbb0c5964de3c7719feb8b89918ac562
Ella <ellamkoch@gmail.com>
2025-11-01 17:23:25 -0500

    updated desktop styles. added in a div in the html to make the desktop work properly. fixed js validation for fields and styling.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ const mainForm =document.getElementById("mortgageForm");
 const amountInput = document.getElementById("amount");
 const termInput = document.getElementById("years");
 const interestInput = document.getElementById("rate");
-const clearAllBtn = document.getElementById('clearAll');
+const typeRadios = document.querySelectorAll('input[name="type"]'); // targets radios for errors
+
+
+
 
 //console.log(js); to check for connection
 
@@ -20,8 +23,15 @@ const totalResult = document.getElementById("totalResult");
 mainForm.addEventListener('submit', (e) => {
     // prevents default action, aka refresh of screen
     e.preventDefault();
+    let hasError = false; // to help make all errors show
     // testing submit
     //console.log("test submit"); // checks submit
+document.getElementById('clearAll').addEventListener('click', function(e) {
+  e.preventDefault();     // stops the link from jumping to top
+  mainForm.reset();       // clears all inputs
+  defaultResults.style.display = 'block';  // show the default results section
+  actualResults.style.display = 'none';    // hide the actual results
+});
 
     // getting the values
     const amount = amountInput.value;
@@ -37,7 +47,7 @@ mainForm.addEventListener('submit', (e) => {
         // shows the error message
         amountInput.parentElement.nextElementSibling.innerText = isAmountValid.message;
         // makes sure the program stops in here. use 'return' keyword to make a function stop.
-        return;
+        hasError = true;
     }
 
     let isTermValid = isInputValid(termInput, 'term'); // checks to see if the term is valid.
@@ -48,7 +58,7 @@ mainForm.addEventListener('submit', (e) => {
             // shows the error message
             termInput.parentElement.nextElementSibling.innerText = isTermValid.message;
             // makes sure the program stops in here. use 'return' keyword to make a function stop.
-            return;
+            hasError = true;
         }
 
         let isInterestValid = isInputValid(interestInput, 'number'); // checks to see if the interest is valid.
@@ -59,9 +69,19 @@ mainForm.addEventListener('submit', (e) => {
                     // shows the error message
                     interestInput.parentElement.nextElementSibling.innerText = isInterestValid.message;
                     // makes sure the program stops in here. use 'return' keyword to make a function stop.
-                    return;
+                    hasError = true;
                 }
+// for radio validation
+        let picked = false;
+        typeRadios.forEach(r => { if (r.checked) picked = true; });
+        if (!picked) {
+        // targets the <p class="input__error"> that sits under the radio container
+        document.querySelector(".choice").parentElement
+            .querySelector(".input__error").textContent = "This field is required";
+       hasError = true;
+        }
 
+    if (hasError) return; // checks to see if fields have errors and then returns errors if they are found. 
     const result = computeMortgage(amount, years, interest);
 
     console.log(result);
@@ -73,7 +93,7 @@ mainForm.addEventListener('submit', (e) => {
     const monthlyPaymentsR = Math.round(result.monthly * 100) / 100;
 
     // want to apply results to the actual text inputs
-    monthlyText.innerText = `£${monthlyPaymentsR.toFixed(2)}`; // adds the £ sign to the beginning. 
+    monthlyText.innerText = `£${monthlyPaymentsR.toFixed(2)}`; // adds the £ sign to the beginning.
     totalResult.innerText = `£${result.total.toFixed(2)}`; // also rounds to 2 decimal points
 
     // displays the results container
@@ -83,15 +103,15 @@ mainForm.addEventListener('submit', (e) => {
 
 // validation function
 function isInputValid(input, type) {
-    let value = input.value; // pulls what the user tyuped.
+    let value = input.value; // pulls what the user typed.
 
-    if (value === "") {// this checks to see if the field is empty
+    if (value === "" ) {// this checks to see if the field is empty
     return   {
         valid: false,
         message: "The field is required",
     };
 }   // checks to see if its supposed to be a number
-    if (type === "number") {
+    if (type === "number" || type === "amount" || type === "term") {// had to add these in to check all the fields for validation
         let numberValue = Number(value); // changes text to a number
         if (numberValue <= 0) {
             return {
@@ -106,4 +126,6 @@ function isInputValid(input, type) {
         message: '',
     };
 }
-
+function isRadioSelected(radios) {
+  return Array.from(radios).some(radio => radio.checked);
+}
